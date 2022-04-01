c5f61b9dec05897fc7ae879ae0237579092ac65d
rwclutterbuck <rwclutterbuck@outlook.com>
2022-04-01 15:13:11 +0100

    Add: socketio

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from flask import Flask
- 
+from flask_socketio import SocketIO, emit 
+import secrets
+
 app = Flask(__name__)
- 
+
+secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = secret_key
+socket = SocketIO(app)
+
 @app.route("/")
 def home_view():
         return "<h1>Hello world</h1>"
+
+@socket.on('connect')
+def test_connect():
+    emit('hello world', {'data': 'hello world'})
+
+
+if __name__ == '__main__':
+    socket.run(app)
