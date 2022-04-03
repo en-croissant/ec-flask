069c3f4dc7d4769268301e1625eff90eb9f1c02d
Noah Platton <noahplatton@gmail.com>
2022-04-03 16:11:05 +0100

    docs(Pipfile): add dependencies flask-cors & werkzeug

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO, emit 
 import secrets
 
 app = Flask(__name__)
+CORS(app)
 
 secret_key = secrets.token_hex(16)
 app.config['SECRET_KEY'] = secret_key
