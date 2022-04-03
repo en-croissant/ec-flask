edf5335f34fc109082d6fa76868f2d22e9901a0e
Noah Platton <noahplatton@gmail.com>
2022-04-03 17:22:39 +0100

    feat: add outline for auth controllers

---
+++ app/controllers/auth.py
@@ -0,0 +1,10 @@
+from werkzeug import exceptions
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# from app.models import User
+
+def login(req):
+  pass
+
+def register(req):
+  pass
