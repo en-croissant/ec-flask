7de9428da02c79b8d21a77c0d2db6037f2b9b363
saminakhan999 <samina.khan9999@gmail.com>
2022-04-04 00:58:14 +0100

    add models for lobby and chat

--- app/models.py
+++ app/models.py
@@ -48,3 +48,51 @@ class Admin(db.Model):
             'email': self.email,
             'password_digest': self.password_digest
     }
+
+class Lobby(db.Model):
+    lobby_id = db.Column(db.Integer, primary_key=True)
+    player_1_key = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    player_2_key = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    history = db.Column(db.String(50000))
+
+    def __init__(self, player_1_key, player_2_key, history):
+        self.player_1_key = player_1_key
+        self.player_2_key = player_2_key
+        self.history = history
+
+    def __repr__(self):
+        return '<id {}>'.format(self.lobby_id)
+    
+    def serialize(self):
+        return {
+            'lobby_id': self.lobby_id,
+            'player_1_key': self.player_1_key, 
+            'player_2_key': self.player_2_key,
+            'history': self.history
+    }
+
+class Chat(db.Model):
+    message_id = db.Column(db.Integer, primary_key=True)
+    lobby_id = db.Column(db.Integer, db.ForeignKey('lobby.lobby_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    message = db.Column(db.String(500))
+    time = db.Column(db.DateTime)
+
+    def __init__(self, user_id, lobby_id, message, time):
+        self.lobby_id = lobby_id
+        self.user_id = user_id
+        self.message = message
+        self.time = time
+
+    def __repr__(self):
+        return '<id {}>'.format(self.message_id)
+    
+    def serialize(self):
+        return {
+            'message_id': self.message_id,
+            'lobby_id': self.lobby_id,
+            'user_id': self.user_id, 
+            'message': self.message,
+            'time': self.time
+    }
+
