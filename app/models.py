9f8d765531a50f48864ecd56aa748ed35fe28e99
Noah Platton <noahplatton@gmail.com>
2022-04-04 11:35:48 +0100

    refactor(models.py): remove admin db and add admin boolean to users table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
+import datetime
+import jwt
+
 from .extensions import db 
-
-
 
 class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
@@ -8,12 +9,14 @@ class Users(db.Model):
     email = db.Column(db.String(100))
     password_digest = db.Column(db.String(100))
     rank = db.Column(db.Integer)
+    admin = db.Column(db.Boolean)
 
-    def __init__(self, username, email, password_digest, rank):
+    def __init__(self, username, email, password_digest, rank, admin=False):
         self.username = username
         self.email = email
         self.password_digest = password_digest
         self.rank = rank
+        self.admin = admin
 
     
     def __repr__(self):
@@ -25,31 +28,10 @@ class Users(db.Model):
             'username': self.username, 
             'email': self.email,
             'password_digest': self.password_digest,
-            'rank': self.rank
+            'rank': self.rank,
+            'admin': self.admin
     }
 
-class Admin(db.Model):
-    admin_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    password_digest = db.Column(db.String(100))
-
-    def __init__(self, username, email, password_digest):
-        self.username = username
-        self.email = email
-        self.password_digest = password_digest
-
-    
-    def __repr__(self):
-        return '<id {}>'.format(self.admin_id)
-    
-    def serialize(self):
-        return {
-            'admin_id': self.admin_id,
-            'username': self.username, 
-            'email': self.email,
-            'password_digest': self.password_digest
-    }
 
 class Lobby(db.Model):
     lobby_id = db.Column(db.Integer, primary_key=True)
