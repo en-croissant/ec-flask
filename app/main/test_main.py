534d08340cfd945c11d7fdf7dd06c2e581c4a66d
saminakhan999 <samina.khan9999@gmail.com>
2022-04-05 13:55:01 +0100

    added get all users test

--- app/main/test_main.py
+++ app/main/test_main.py
@@ -1,4 +1,10 @@
 def test_home(api):
     """connects to root route"""
     app = api.get('/')
+    assert app.status == '200 OK'
     assert app.get_data() == b'<h1>Hello world</h1>'
+
+def test_get_allusers(api):
+    app = api.get('/users')
+    assert app.status == '200 OK'
+    assert b'user_id' in app.get_data()
