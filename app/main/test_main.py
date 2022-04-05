ac4fd7e908a1ef201eba41a137e68c4832f04a48
Noah Platton <noahplatton@gmail.com>
2022-04-05 16:30:39 +0100

    prepare for merge"

--- app/main/test_main.py
+++ app/main/test_main.py
@@ -1,4 +1,5 @@
 def test_home(api):
     """connects to root route"""
-    app = api.get('/')
-    assert app.get_data() == b'<h1>Hello world</h1>'
+    res = api.get('/')
+    assert res.get_data() == b'<h1>Hello world</h1>'
+
