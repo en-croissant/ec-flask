cb493b15afc0b922c7445484ce894a5f143a4112
rwclutterbuck <rwclutterbuck@outlook.com>
2022-04-07 01:40:28 +0100

    Add: persistence between users

--- app/main/socket.py
+++ app/main/socket.py
@@ -4,6 +4,7 @@ import chess
 from app.models import Lobby
 
 def chess_game():
+    board = dict()
     @socketio.on('connect')
     def test_connect():
         emit('hello world', {'data': 'hello world'})
@@ -11,6 +12,7 @@ def chess_game():
     @socketio.on('join')
     def test_join(data):
         # when user joins a room they get the latest board state
+        
         room = data["lobby_id"]
         join_room(room)
         
@@ -21,35 +23,34 @@ def chess_game():
         #player_2 = match_info['player_2_key']
 
         # Get latest board state
-        # board = chess.Board()
-        # emit('inital board', {'board':board.board_fen()})
-
+        if room not in board.keys():
+            board[room] = chess.Board()
+        emit('initial board', {'board':board[room].fen()})
         # [TODO] Checks if it is user's turn to move
         #data['user_id']
 
         print("user joined room /play")
 
-    @socketio.on('move piece')
-    def test_move(data):
-        print(f"moving to {data['move']}")
-        room = data['lobby_id']
-        # [TODO] Check if it is the player's turn
+        @socketio.on('move piece')
+        def test_move(data):
+            print(f"moving to {data['move']}")
+            # [TODO] Check if it is the player's turn
 
-        # Save move to history
+            # Save move to history
 
-        # board.push_san(data['move'])
-        # [NOTE] Check how to format match history correctly
-        #Lobby.query.get({"lobby_id":room}).update({"history":f"{match_history},{data['data']}"})
+            board[room].push_san(data['move']['san'])
+            # [NOTE] Check how to format match history correctly
+            #Lobby.query.get({"lobby_id":room}).update({"history":f"{match_history},{data['data']}"})
 
-        # Check for checkmate
-        # if board.is_checkmate():
-        #     # end game
-        #     pass
-        # Broadcasts move to all users in room
-        emit('opponent move', {'chessMove': data['move']}, to=room, include_self=False)
+            # Check for checkmate
+            # if board.is_checkmate():
+            #     # end game
+            #     pass
+            # Broadcasts move to all users in room
+            emit('opponent move', {'chessMove': data['move']}, to=room, include_self=False)
 
-        # @socketio.on('reset board')
-        # def test_reset_board():
-        #     board.reset()
-        #     emit('new board', {'chessboard': board.board_fen()})
+            # @socketio.on('reset board')
+            # def test_reset_board():
+            #     board.reset()
+            #     emit('new board', {'chessboard': board.board_fen()})
 
